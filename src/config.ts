5a6703720edec12ff116a805fdfac5adb6cc07f0
jouycee <ukachukwuuwadi@gmail.com>
2024-11-26 13:11:46 +0100

    Update config.ts
    Meta title
    meta description

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Uwadi Joyce | Digital marketer specialist';
+export const SITE_DESCRIPTION = 'An experienced digital marketer skilled in SEO,social media strategy and content creation helping businesses grow online.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
